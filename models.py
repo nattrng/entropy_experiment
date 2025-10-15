6daefbce9c05554aa47e680ea4611454838b3dba
nattrng <nathanngtruong@gmail.com>
2025-10-14 22:17:55 -0700

    Add files via upload

---
+++ models.py
@@ -0,0 +1,8 @@
+"""Insert models here"""
+
+model_list = [
+    'NousResearch/Hermes-4-70B',
+    'deepseek-ai/DeepSeek-R1-Distill-Llama-70B',
+    'mistralai/Mistral-Small-3.2-24B-Instruct-2506',
+    'baidu/Qianfan-VL-70B'
+]
