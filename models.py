06233ff97922a76f5a15143671cf86508de6e35c
nattrng <nathanngtruong@gmail.com>
2025-10-21 14:51:20 -0700

    Update model list in models.py

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 """Insert models here"""
 
 model_list = [
-    'NousResearch/Hermes-4-70B',
-    'deepseek-ai/DeepSeek-R1-Distill-Llama-70B',
+    'openai/gpt-oss-20b',
+    'unsloth/DeepSeek-R1-Distill-Llama-70B-bnb-4bit',
     'mistralai/Mistral-Small-3.2-24B-Instruct-2506',
-    'baidu/Qianfan-VL-70B'
+    'Qwen/Qwen3-Omni-30B-A3B-Instruct'
 ]
